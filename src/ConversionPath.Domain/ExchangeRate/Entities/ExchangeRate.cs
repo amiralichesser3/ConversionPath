e62152d23f7c6316ef379752c18191a1dea20731
amirali safari <safari.amirali.asa@gmail.com>
2022-05-29 02:13:53 +0430

    Initial Commit

---
+++ src/ConversionPath.Domain/ExchangeRate/Entities/ExchangeRate.cs
@@ -0,0 +1,19 @@
+﻿using ConversionPath.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionPath.Domain.ExchangeRate.Entities
+{
+    public class ExchangeRate: BaseEntity
+    {
+        public double Rate { get; set; }
+        public string SourceCurrency { get; set; }
+        public string DestinationCurrency { get; set; }
+        public string 
+        public string Source { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
